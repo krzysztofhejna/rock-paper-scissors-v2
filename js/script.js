996743b737ee56d5be25e43ab69cbe589a246b81
krzysztofhejna <khejna15@wp.pl>
2018-11-28 19:03:34 +0100

    Update Javascript code

--- js/script.js
+++ js/script.js
@@ -1,95 +1,25 @@
 'use strict';
 (function () {
     var pointsToWin;
-    var button1 = document.getElementById('newGame-btn');
-    var buttonRock = document.getElementById('rock-btn');
-    var buttonPaper = document.getElementById('paper-btn');
-    var buttonScissors = document.getElementById('scissors-btn');
+    var button1 = document.getElementById("newGame-btn");
+    var buttonRock = document.getElementById("rock");
+    var buttonPaper = document.getElementById("paper");
+    var buttonScissors = document.getElementById("scissors");
     var userScore = 0;
     var computerScore = 0;
+    var userScoreSpan = document.getElementById("user-score");
+    var computerScoreSpan = document.getElementById("computer-score");
+    var output = document.getElementById("output");
 
 
     var isInputValid = function (pointsToWin) {
         if (!pointsToWin || isNaN(pointsToWin) || pointsToWin < 1 || pointsToWin > 10) {
             return alert('Please enter a number between 1 and 10.');
         }
-
         return true;
     }
 
-    var isGameFinished = function(Score) {
-        if (Score >= pointsToWin) {
-            return true;
-        }
-        
-        return false;
-    }
-
-    var playerMove = function (playerPick) {
-        var userScoreSpan = document.getElementById('user-score');
-        var computerScoreSpan = document.getElementById("computer-score");
-        var output = document.getElementById('output');
-        var opponentPick = function (){
-            switch (Math.floor(Math.random() * 3) + 1) {
-                case 1:
-                    return 'rock';
-                case 2:
-                    return 'paper';
-                case 3:
-                    return 'scissors';
-            };
-        }
-
-        var win = function() {
-            userScore++;
-            userScoreSpan.innerHTML = userScore;
-            if (isGameFinished(userScore)) {
-                return alert('dd');
-            }
-        }
-
-        var lose = function() {
-            computerScore++;
-            computerScoreSpan.innerHTML = computerScore;
-        }
-
-        var draw = function() {
-            userScoreSpan.innerHTML = userScore;
-            computerScoreSpan.innerHTML = computerScore;
-        }
-
-        switch (playerPick + opponentPick()) {
-            case "rockrock":
-                draw();
-                return output.innerHTML = ('You both chose ROCK. It is a tie.');
-            case "paperpaper":
-                draw();
-                return output.innerHTML = ('You both chose PAPER. It is a tie.');
-            case "scissorsscissors":
-                draw();
-                return output.innerHTML = ('You both chose SCISSORS. It is a tie.');
-            case "rockpaper":
-                lose();
-                return output.innerHTML = ('You played ROCK, opponent played PAPER. You lost.');
-            case "rockscissors":
-                win();
-                return output.innerHTML = ('You played ROCK, opponent played SCISSORS. You won.');
-            case "paperscissors":
-                lose();
-                return output.innerHTML = ('You played PAPER, opponent played SCISSORS. You lost.');
-            case "paperrock":
-                win();
-                return output.innerHTML = ('You played PAPER, opponent played ROCK. You won.');
-            case "scissorsrock":
-                lose();
-                return output.innerHTML = ('You played SCISSORS, opponent played ROCK. You lost.');
-            case "scissorspaper":
-                win();
-                return output.innerHTML = ('You played SCISSORS, opponent played PAPER. You won.');
-        }
-    }
-
-    button1.addEventListener('click', function() {
+    var newGame = function () {
         pointsToWin = window.prompt ('Amount of points needed to win the game? Max-10');
         if (isInputValid(pointsToWin)) {
             document.getElementById('newGame-btn').style.display = "none";
@@ -99,8 +29,118 @@
             for (var i=0; i < buttons.length; i++) {
                 buttons[i].style.display = "inline-block";
             }
+            activateButtons();
+            userScore = 0;
+            userScoreSpan.innerHTML = userScore;
+            computerScore = 0;
+            computerScoreSpan.innerHTML = computerScore;
+            output.innerHTML = ('');
         }
-    });
+    }
+    
+    var isGameFinished = function(Score) {
+        if (Score >= pointsToWin) {
+            return true;
+        }
+        return false;
+    }
+
+    var disableButtons = function() {
+    	buttonRock.disabled = true;
+    	buttonPaper.disabled = true;
+    	buttonScissors.disabled = true;
+    }
+    
+    var activateButtons = function() {
+        buttonRock.disabled = false;
+    	buttonPaper.disabled = false;
+    	buttonScissors.disabled = false;
+    }
+
+    var opponentPick = function (){
+        switch (Math.floor(Math.random() * 3) + 1) {
+            case 1:
+                return 'rock';
+            case 2:
+                return 'paper';
+            case 3:
+                return 'scissors';
+        }
+    }
+
+    var win = function(playerPick) {
+        userScore++;
+        userScoreSpan.innerHTML = userScore;
+        document.getElementById(playerPick).classList.add('green-border');
+        setTimeout(function() {document.getElementById(playerPick).classList.remove('green-border')}, 500);
+        if (isGameFinished(userScore)) {
+            output.innerHTML = ('You WON. Press "Start a New Game" to play again!');
+            document.getElementById("newGame-btn").style.display = "block";
+            disableButtons();
+        }
+    }
+
+    var lose = function(playerPick) {
+        computerScore++;
+        computerScoreSpan.innerHTML = computerScore;
+        document.getElementById(playerPick).classList.add('red-border');
+        setTimeout(function() {document.getElementById(playerPick).classList.remove('red-border')}, 500);
+        if (isGameFinished(computerScore)) {
+            output.innerHTML = ('You LOST. Press "Start a New Game" to play again!');
+            document.getElementById("newGame-btn").style.display = "block";
+            disableButtons();
+        }
+    }
+
+    var draw = function(playerPick) {
+        userScoreSpan.innerHTML = userScore;
+        computerScoreSpan.innerHTML = computerScore;
+        document.getElementById(playerPick).classList.add('gray-border');
+        setTimeout(function() {document.getElementById(playerPick).classList.remove('gray-border')}, 500);
+    }
+
+    var playerMove = function(playerPick) {
+        switch (playerPick + opponentPick()) {
+            case "rockrock":
+                output.innerHTML = ('You both chose ROCK. It is a tie.');
+                draw(playerPick);
+                break;
+            case "paperpaper":
+                output.innerHTML = ('You both chose PAPER. It is a tie.');
+                draw(playerPick);
+                break;
+            case "scissorsscissors":
+                output.innerHTML = ('You both chose SCISSORS. It is a tie.');
+                draw(playerPick);
+                break;
+            case "rockpaper":
+                output.innerHTML = ('You played ROCK, opponent played PAPER. You lost.');
+                lose(playerPick);
+                break;
+            case "rockscissors":
+                output.innerHTML = ('You played ROCK, opponent played SCISSORS. You won.');
+                win(playerPick);
+                break;
+            case "paperscissors":
+                output.innerHTML = ('You played PAPER, opponent played SCISSORS. You lost.');
+                lose(playerPick);
+                break;
+            case "paperrock":
+                output.innerHTML = ('You played PAPER, opponent played ROCK. You won.');
+                win(playerPick);
+                break;
+            case "scissorsrock":
+                output.innerHTML = ('You played SCISSORS, opponent played ROCK. You lost.');
+                lose(playerPick);
+                break;
+            case "scissorspaper":
+                output.innerHTML = ('You played SCISSORS, opponent played PAPER. You won.');
+                win(playerPick);
+                break;
+        }
+    }
+
+    button1.addEventListener('click', newGame);
 
     buttonRock.addEventListener('click', function() {
         playerMove("rock");
