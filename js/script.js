0deb9280b84a9959eb039a51502dbeabea4f403a
krzysztofhejna <khejna15@wp.pl>
2018-12-06 23:04:55 +0100

    Add new game modal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 'use strict';
 (function () {
+    var buttonNewGame = document.getElementById("newGame-btn");
+    var startButton = document.getElementById("formButton");
+    var roundLimit = document.getElementById("inputRoundLimit");
+    var buttons = document.querySelectorAll('.game-btn');
+    var userScoreSpan = document.getElementById("user-score");
+    var computerScoreSpan = document.getElementById("computer-score");
+    var output = document.getElementById("output");
+
+    var modals = document.querySelectorAll('.modal');
+    var modalNewGame = document.getElementById("modal-newGame")
+    var modalScore = document.getElementById("modal-score");
+    var modalOutput = document.getElementById("modal-score__output");
+    var closeButtons = document.querySelectorAll('.modal .close');
+    var modalTableBody = document.getElementById('game-table__body');
+
     var params = {
-            pointsToWin: null,
-            buttonNewGame: document.getElementById("newGame-btn"),
-            buttons: document.querySelectorAll('.game-btn'),
-            userScore: 0,
-            computerScore: 0,
-            userScoreSpan: document.getElementById("user-score"),
-            computerScoreSpan: document.getElementById("computer-score"),
-            output: document.getElementById("output")
+        pointsToWin: null,
+        roundCount: 0,
+        userScore: 0,
+        computerScore: 0,
+        playerChoice: undefined,
+        computerChoice: undefined,
+        roundResult: undefined,
+        progress: []
+    }
+
+    var gameData = {
+        roundNmbr: undefined,
+        playerMv: undefined,
+        computerMv: undefined,
+        roundRslt: undefined,
+        gameScr: undefined
     }
 
     var isInputValid = function (pointsToWin) {
@@ -19,21 +42,23 @@
     }
 
     var newGame = function () {
-        params.pointsToWin = window.prompt ('Amount of points needed to win the game? Max-10');
+        params.pointsToWin = roundLimit.value;
         if (isInputValid(params.pointsToWin)) {
             document.getElementById('newGame-btn').style.display = "none";
             document.getElementById('scoreboard').style.display = "block";
             document.getElementById('memo').style.display = "block";
-            for (var i=0; i < params.buttons.length; i++) {
-                params.buttons[i].style.display = "inline-block";
+            for (var i=0; i < buttons.length; i++) {
+                buttons[i].style.display = "inline-block";
             }
             activateButtons();
+            params.roundCount = 0;
+            params.progress = [];
             params.userScore = 0;
-            params.userScoreSpan.innerHTML = params.userScore;
+            userScoreSpan.innerHTML = params.userScore;
             params.computerScore = 0;
-            params.computerScoreSpan.innerHTML = params.computerScore;
-            while (params.output.hasChildNodes ()) {
-                params.output.removeChild(params.output.firstChild);
+            computerScoreSpan.innerHTML = params.computerScore;
+            while (output.hasChildNodes ()) {
+                output.removeChild(output.firstChild);
             }
         }
     }
@@ -46,17 +71,86 @@
     }
 
     var disableButtons = function() {
-    	for (i = 0; i < params.buttons.length; i++) {
-            params.buttons[i].disabled = true;
+    	for (i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
         }
     }
     
     var activateButtons = function() {
-        for (i = 0; i < params.buttons.length; i++) {
-            params.buttons[i].disabled = false;
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = false;
         }
     }
 
+    var playerMove = function(playerPick) {
+        params.roundCount = ++params.roundCount;
+        params.playerChoice = playerPick;
+        switch (playerPick + opponentPick()) {
+            case "rockrock":
+                output.innerHTML = ('You both chose ROCK. It is a tie.');
+                params.computerChoice = 'rock';
+                params.roundResult = 'draw';
+                draw(playerPick);
+                break;
+            case "paperpaper":
+                output.innerHTML = ('You both chose PAPER. It is a tie.');
+                params.computerChoice = 'paper';
+                params.roundResult = 'draw';
+                draw(playerPick);
+                break;
+            case "scissorsscissors":
+                output.innerHTML = ('You both chose SCISSORS. It is a tie.');
+                params.computerChoice = 'scissors';
+                params.roundResult = 'draw';
+                draw(playerPick);
+                break;
+            case "rockpaper":
+                output.innerHTML = ('You played ROCK, opponent played PAPER. You lost.');
+                params.computerChoice = 'paper';
+                params.roundResult = 'loss';
+                lose(playerPick);
+                break;
+            case "rockscissors":
+                output.innerHTML = ('You played ROCK, opponent played SCISSORS. You won.');
+                params.computerChoice = 'scissors';
+                params.roundResult = 'win';
+                win(playerPick);
+                break;
+            case "paperscissors":
+                output.innerHTML = ('You played PAPER, opponent played SCISSORS. You lost.');
+                params.computerChoice = 'scissors';
+                params.roundResult = 'loss';
+                lose(playerPick);
+                break;
+            case "paperrock":
+                output.innerHTML = ('You played PAPER, opponent played ROCK. You won.');
+                params.computerChoice = 'rock';
+                params.roundResult = 'win';
+                win(playerPick);
+                break;
+            case "scissorsrock":
+                output.innerHTML = ('You played SCISSORS, opponent played ROCK. You lost.');
+                params.computerChoice = 'rock';
+                params.roundResult = 'loss';
+                lose(playerPick);
+                break;
+            case "scissorspaper":
+                output.innerHTML = ('You played SCISSORS, opponent played PAPER. You won.');
+                params.computerChoice = 'paper';
+                params.roundResult = 'win';
+                win(playerPick);
+                break;
+        }
+        gameData = {
+            roundNmbr: params.roundCount,
+            playerMv: params.playerChoice,
+            computerMv: params.computerChoice,
+            roundRslt: params.roundResult,
+            gameScr: params.userScore + ':' + params.computerScore 
+        }
+        params.progress.push(gameData);
+    }
+    
     var opponentPick = function (){
         switch (Math.floor(Math.random() * 3) + 1) {
             case 1:
@@ -67,14 +161,17 @@
                 return 'scissors';
         }
     }
-
+    
     var win = function(playerPick) {
         params.userScore++;
-        params.userScoreSpan.innerHTML = params.userScore;
+        userScoreSpan.innerHTML = params.userScore;
         document.getElementById(playerPick).classList.add('green-border');
         setTimeout(function() {document.getElementById(playerPick).classList.remove('green-border')}, 500);
         if (isGameFinished(params.userScore)) {
-            params.output.innerHTML = ('You WON. Press "Start a New Game" to play again!');
+            setTimeout (function () {
+                showModal(modalScore);
+            }, 500);
+            modalOutput.innerHTML = ('You WON. Press "Start a New Game" to play again!');
             document.getElementById("newGame-btn").style.display = "block";
             disableButtons();
         }
@@ -82,69 +179,84 @@
 
     var lose = function(playerPick) {
         params.computerScore++;
-        params.computerScoreSpan.innerHTML = params.computerScore;
+        computerScoreSpan.innerHTML = params.computerScore;
         document.getElementById(playerPick).classList.add('red-border');
         setTimeout(function() {document.getElementById(playerPick).classList.remove('red-border')}, 500);
         if (isGameFinished(params.computerScore)) {
-            params.output.innerHTML = ('You LOST. Press "Start a New Game" to play again!');
+            setTimeout (function () {
+                showModal(modalScore);
+            }, 500);
+            modalOutput.innerHTML = ('You LOST. Press "Start a New Game" to play again!');
             document.getElementById("newGame-btn").style.display = "block";
             disableButtons();
         }
     }
 
     var draw = function(playerPick) {
-        params.userScoreSpan.innerHTML = params.userScore;
-        params.computerScoreSpan.innerHTML = params.computerScore;
+        userScoreSpan.innerHTML = params.userScore;
+        computerScoreSpan.innerHTML = params.computerScore;
         document.getElementById(playerPick).classList.add('gray-border');
         setTimeout(function() {document.getElementById(playerPick).classList.remove('gray-border')}, 500);
     }
+    
+    var showModal = function(modal){
+		modal.classList.add('show');
+        document.querySelector('#modal-overlay').classList.add('show');
+        createGameTable(params.progress);
+	};
 
-    var playerMove = function(playerPick) {
-        switch (playerPick + opponentPick()) {
-            case "rockrock":
-                params.output.innerHTML = ('You both chose ROCK. It is a tie.');
-                draw(playerPick);
-                break;
-            case "paperpaper":
-                params.output.innerHTML = ('You both chose PAPER. It is a tie.');
-                draw(playerPick);
-                break;
-            case "scissorsscissors":
-                params.output.innerHTML = ('You both chose SCISSORS. It is a tie.');
-                draw(playerPick);
-                break;
-            case "rockpaper":
-                params.output.innerHTML = ('You played ROCK, opponent played PAPER. You lost.');
-                lose(playerPick);
-                break;
-            case "rockscissors":
-                params.output.innerHTML = ('You played ROCK, opponent played SCISSORS. You won.');
-                win(playerPick);
-                break;
-            case "paperscissors":
-                params.output.innerHTML = ('You played PAPER, opponent played SCISSORS. You lost.');
-                lose(playerPick);
-                break;
-            case "paperrock":
-                params.output.innerHTML = ('You played PAPER, opponent played ROCK. You won.');
-                win(playerPick);
-                break;
-            case "scissorsrock":
-                params.output.innerHTML = ('You played SCISSORS, opponent played ROCK. You lost.');
-                lose(playerPick);
-                break;
-            case "scissorspaper":
-                params.output.innerHTML = ('You played SCISSORS, opponent played PAPER. You won.');
-                win(playerPick);
-                break;
+    var createGameTable = function(results) {
+        while (modalTableBody.hasChildNodes ()) {
+            modalTableBody.removeChild(modalTableBody.firstChild);
+        }
+        for (var i = 0; i < results.length; i++) {
+            var newRow = modalTableBody.insertRow(i);
+            modalTableBody.insertBefore(newRow, modalTableBody.firstChild);
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
+            var cell4 = newRow.insertCell(3);
+            var cell5 = newRow.insertCell(4);
+            cell1.innerHTML = results[i].roundNmbr;
+            cell2.innerHTML = results[i].playerMv;
+            cell3.innerHTML = results[i].computerMv;
+            cell4.innerHTML = results[i].roundRslt;
+            cell5.innerHTML = results[i].gameScr;
         }
     }
 
-    params.buttonNewGame.addEventListener('click', newGame);
+    var closeModal = function(){
+        for (var i = 0; i < modals.length; i++) {
+            modals[i].classList.remove('show');
+        }
+        document.querySelector('#modal-overlay').classList.remove('show');
+    }
 
-    for (var i = 0; i < params.buttons.length; i++) {
-        params.buttons[i].addEventListener('click', function() {
+    buttonNewGame.addEventListener('click', function() {
+        showModal(modalNewGame);
+    });
+
+    startButton.addEventListener('click', function(){
+        event.preventDefault();
+        newGame();
+        closeModal();
+    });
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
             playerMove(this.getAttribute("data-move"));
         });
     }
+
+    for(var i = 0; i < closeButtons.length; i++){
+        closeButtons[i].addEventListener('click', closeModal);
+    }
+
+    document.querySelector('#modal-overlay').addEventListener('click', closeModal);
+
+	for(var i = 0; i < modals.length; i++){
+		modals[i].addEventListener('click', function(event){
+			event.stopPropagation();
+		});
+	}
 })();
